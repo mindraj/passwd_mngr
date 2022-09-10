ed194e01f4cfb05a5910f1ef8ac1011ceb3f6330
mindraj <m1ndraj@ya.ru>
2022-09-10 06:05:54 +0500

    Initial commit
    Changes to be committed:
          new file:   passwords.c

---
+++ passwords.c
@@ -0,0 +1,356 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdbool.h>
+
+#define strip_trailing_nl(string) string[strlen(string) - 1] = '\0'
+#define clear_input_buffer                                                \
+		{                                                         \
+			int cs;                                           \
+			while ((cs = getchar()) != '\n' && cs != EOF) {}  \
+		} 
+
+#if defined(__linux__) || defined(__FreeBSD__) || defined(__CYGWIN__)
+char *make_passwd(int passwd_length)
+{
+	FILE *random = fopen("/dev/urandom", "r");
+	char *passwd = malloc(passwd_length);
+	unsigned char random_byte = 0;
+	for (int i = 0; i < passwd_length;)
+	{
+		random_byte = (unsigned char) getc(random);
+		if (isalnum(random_byte))
+		{
+			passwd[i] = random_byte;
+			i++;
+		}
+	}
+	fclose(random);
+	return passwd;
+}
+#else
+	#error Not a supported platform.
+#endif
+
+void *ask_info(char *prompt, char *type, int length, int (*evaluation_function)(void *))
+{
+	/* Generic ask the user function
+	 *
+	 * Prompt will be displayed before
+	 * the input field.
+	 *
+	 * Supports the following types:
+	 * "i" - long int
+	 * "s" - char *
+	 *
+	 * For integers lenth argument is ingored.
+	 * Strings will be saved up to specified lenth, 
+	 * or dynamically allocated to any size if
+	 * lenth is set to 0.
+	 *
+	 * Evaluation function is there when you need
+	 * to discard certain types of input.
+	 * The input collected by this fuction is passed to
+	 * evaluation function before being returned.
+	 * If evaluation function function returns 0,
+	 * the input is discarded and NULL is returned by 
+	 * this function otherwise if any non-0 value is returned
+	 * the function returns pointer to the value gethered from the user
+	 *
+	 * Pass NULL as evaluation_function if you do not
+	 * need this functionality.
+	 */
+
+	void *result;
+
+	fputs(prompt, stdout);
+	if (strcmp(type, "i") == 0)
+	{
+		char *number_string = malloc(10);
+		memset(number_string, '\0', 10);
+
+		result = malloc(sizeof(long int));
+		
+		bool success = false;
+		while (success == false)
+		{
+			int ch;
+
+			if (strlen(number_string) == 10)
+			{
+				number_string = realloc(number_string, (strlen(number_string) + 10));
+			}
+
+			ch = getchar();
+			if (ch == '\\')
+			{
+				if (getchar() == 'q')
+				{
+					exit(EXIT_SUCCESS);
+				}
+				else
+				{
+					printf("\n%s", prompt);
+					clear_input_buffer;
+				}
+			}
+			else if (ch == '\n' || ch == EOF)
+			{
+				success = true;
+				number_string[strlen(number_string)] = '\0';
+			}
+			else if (isdigit(ch) == 0)
+			{
+				printf("\n%s", prompt);
+				clear_input_buffer;
+			}
+			else 
+			{
+				number_string[strlen(number_string)] = ch;
+			}
+		}
+
+		*(long int *) result = atoi(number_string);
+	}
+	else if (strcmp(type, "s") == 0) {}
+
+	if (evaluation_function == NULL)
+		return result;
+	else 
+		return (evaluation_function(result) == 0 ? NULL : result);
+}
+
+
+
+char *find_passwd(char *identity, FILE *passwd_file)
+{
+	char saved_name[strlen(identity)];
+	memset(saved_name, '\0', strlen(identity));
+
+	char *passwd = malloc(50);
+	
+	// Find
+	for (;;)
+	{
+		int ch;
+		memset(saved_name, '\0', strlen(saved_name)); /* clear saved_name */
+
+		// Peek to check that next up isn't EOF
+		if ((ch = fgetc(passwd_file)) == EOF)
+		{
+			return NULL;
+		}
+		else
+		{
+			ungetc(ch, passwd_file);
+		}
+
+		// Save the identity name to a string
+		for (int i = 0; (ch = fgetc(passwd_file)) != '\t';i++)
+		{
+			saved_name[i] = ch;
+		}
+		
+		// If found matching identity name - break
+		if (strcmp(saved_name, identity) == 0)
+			break;
+
+		// Skip to the next line
+		for (int ch; fgetc(passwd_file) != '\n';) {}
+
+	}
+
+	// Read password string
+	for (int i = 0, ch; (ch = fgetc(passwd_file)) != '\n'; i++)
+	{
+		if (i == 50)
+			passwd = realloc(passwd, i + 50);
+
+		passwd[i] = ch;
+	}
+	
+	// Very important to rewind, otherwise subsequent operations
+	// with the file will fail.
+	rewind(passwd_file);
+
+	return passwd;
+}
+
+char *get_identity_name(void)
+{
+	char *identity = malloc(100);
+	for (bool success = false; success == false;)
+	{
+		fputs("Identity name? ", stdout);
+		success = true;
+		for (int ch, i = 0; (ch = getchar()) != '\n'; i++)
+		{
+			if (i == 100)
+				identity = realloc(identity, i + 100);
+
+			if (ch == '\t')
+			{
+				puts("Tabs are not permitted in identity names!");
+				success = false;
+				break;
+			}
+			else 
+			{
+				identity[i] = (char) ch;
+			}
+		}
+	}
+
+	return identity;
+}
+
+void append_passwd_file(FILE *passwd_file)
+{
+
+	// Declare variables
+	long int *passwd_length;
+	char *identity;
+	
+	// Get identity name
+	for (;;)
+	{
+		identity = get_identity_name();
+		puts(identity);
+		if (find_passwd(identity, passwd_file) == NULL)
+		{
+			break;
+		}
+		else
+		{
+			puts("\nPassword for identity already present\n");
+			free(identity);
+		}
+	}
+	
+	// Generate password
+	
+	// Ask for password length until password lenth is obtained
+	passwd_length = 
+		ask_info("How long should the password be? ", "i", 0, NULL);
+		
+	char *passwd = make_passwd(*passwd_length);
+
+	// Put everything into the file
+	// and password in stdout
+	fprintf(passwd_file, "%s\t%s\n", identity, passwd);
+	printf("The password for %s is\n\n%s\n\n", identity, passwd);
+
+	exit(EXIT_SUCCESS);
+}
+
+void query_passwd_file(FILE *passwd_file)
+{
+	
+	// Get identity name
+	char *identity = get_identity_name();
+
+	// Find password or fail
+	char *passwd = find_passwd(identity, passwd_file);
+	if (passwd == NULL)
+	{
+		fprintf(stderr, "\nIdentity not found!\n\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// Print
+	printf("Password for %s:\n\n%s\n\n", identity, passwd);
+	
+	exit(EXIT_SUCCESS);
+}
+
+
+int main(void)
+{
+	// Greeting
+	puts("Welcome to Mindraj's Password Management Wizzard (MiPMM)!\n"
+			"Enter \\q to any prompt to quit\n");
+
+	bool success = false;
+
+	// Ask service
+	char service[200];
+service_query_loop_start:
+	fputs("What service? (\\q to quit) ", stdout);
+	fgets(service, 200, stdin);
+	strip_trailing_nl(service);
+	if (strcmp("\\q", service) == 0)
+		exit(EXIT_SUCCESS);
+
+	// Make filename
+	char *passwd_file_name = malloc(strlen(service) + strlen(".passwd"));
+	sprintf(passwd_file_name, "%s.passwd", service);	
+
+	// Scan folder for passwd files
+#if defined(__linux__) || defined(__FreeBSD__) || defined(__CYGWIN__)
+	{
+		DIR *program_directory;
+		struct dirent *dirfile;
+		bool found_passwd = false;
+
+		program_directory = opendir("./");
+
+		while ((dirfile = readdir(program_directory)) != NULL)
+		{
+			if (strcmp(dirfile->d_name, passwd_file_name) == 0)
+			{
+				found_passwd = true;
+				break;
+			}
+		}
+#else
+	#error Not a supported platform.
+#endif
+
+		if (!found_passwd)
+		{
+			for (;;)
+			{
+				fprintf(stderr,
+						"Service %s not found. Add it? (yes/no) ",
+						service);
+
+				char answer[5];
+				fgets(answer, 5, stdin);
+				strip_trailing_nl(answer);
+
+				if (strcmp(answer, "yes") == 0)
+					break;
+				else if (strcmp(answer, "no") == 0)
+					goto service_query_loop_start;
+				else if (strcmp(answer, "\\q") == 0)
+					exit(EXIT_SUCCESS);
+			}
+		}
+	}
+
+	// Ask operation
+	char operation[10];
+	for (bool success = false; success == false;)
+	{
+		success = true;
+		fputs("(r)ead or (w)rite? ", stdout);
+		fgets(operation, 6, stdin);
+		strip_trailing_nl(operation);
+
+		if((strcmp(operation, "r") == 0) || (strcmp(operation, "read") == 0))
+			query_passwd_file(fopen(passwd_file_name, "r"));
+		else if((strcmp(operation, "w") == 0) || (strcmp(operation, "write") == 0))
+			append_passwd_file(fopen(passwd_file_name, "a+"));
+		else if(strcmp(operation, "\\q") == 0)
+			exit(EXIT_SUCCESS);
+		else
+		{
+			puts("Not a valid operation");
+			success = false;
+		}
+		clear_input_buffer;
+	}
+}
