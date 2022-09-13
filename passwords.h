4907eb626eb2142d549c203e191c22f36345b2b4
mindraj <m1ndraj@ya.ru>
2022-09-13 12:09:08 +0500

    Renamed header file

---
+++ passwords.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdbool.h>
+#include <sodium.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define strip_trailing_nl(string) string[strlen(string) - 1] = '\0'
+#define clear_input_buffer                                    \
+		{                                                     \
+			int cs;                                           \
+			while ((cs = getchar()) != '\n' && cs != EOF) {}  \
+		} 
+
+extern void *ask_info (char *prompt, char *type, int length, int (*evaluation_function)(void *));
+unsigned char *get_enc_key(void);
